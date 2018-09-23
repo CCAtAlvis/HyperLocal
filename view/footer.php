58c8d544b56edad28da7fe1d1aa27ec76bf8c7f0
Chinmay Chandak <chandakchinmay@gmail.com>
2018-09-24 00:50:01 +0530

    added include files for view
    header.php: contains <head> and the header portion of the view
    i.e. navbar and other mandatory stuff
    footer.php: will contain the footer of view and JS files
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+<?php  ?>
+
+
+    <!-- body of the page ends here -->
+
+    <!-- include all the script below in order of dependancy -->
+    <!-- <script src="main.js"></script> -->
+
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+
+    <!-- meta tags follows -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- link all the required css files below -->
+    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+</head>
+<body>
+
+<!-- actual body of the page follows -->
