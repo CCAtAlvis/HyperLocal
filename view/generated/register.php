3d5199ef0476e4f6d0b445040fac53b4751dbee5
Chinmay Chandak <chandakchinmay@gmail.com>
2018-10-02 17:48:23 +0530

    changed dir structure
    generated views i.e. phps are now stored in
    ./view/generated
    except for header.php and footer.php
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ view/generated/register.php
@@ -0,0 +1,28 @@
+<?php
+    $title = 'Register';
+    include_once 'view/header.php';
+?>
+
+<link rel="stylesheet" href="./view/static/css/home.css" />
+
+<div id="body">
+    <div class="left-panel panels">
+        <h1 id="heading">Build your own <br>community.</h1>
+        <p id="para">Connect with people around you. Easily.</p>
+        <div id="link"><a>Know more</a></div>
+    </div>
+
+    <div class="right-panel panels">
+        <div class="card">
+            <p class="question">Where can i eat awesome pizzzza?</p>
+            <div class="bottom-bar">
+                <div class="location">Nerul &bull; 2km </div>
+                <div class="ans-button"><a>Answer <span calss="arrow">-></span></a></div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    include_once 'view/footer.php';
+?>
