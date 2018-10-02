d5e2619a51adc257c38e8467a304de0581a348cf
Chinmay Chandak <chandakchinmay@gmail.com>
2018-10-02 17:46:49 +0530

    added style and script loader
    works same as view loader
    
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

--- include.php
+++ include.php
@@ -9,13 +9,51 @@ spl_autoload_register(function ($class) {
 
 function LoadView(string $view)
 {
-    $loadView = 'view/'.$view.'.php';
+    $loadView = 'view/generated/'.$view.'.php';
     if (is_file($loadView)) {
         include $loadView;
     } else {
-        die('View not found');
+        die("View not found $loadView");
         // show 404 here
     }
+}
+
+// pass in the array of stylesheets, 
+// name must be same as that of stylesheet without extension
+function LoadStyling(array $styles) : string
+{
+    $html = '';
+    foreach($styles as $style) {
+        $loadStyle = 'view/static/css/'.$style.'.css';
+        if (is_file($loadStyle)) {
+            $html .= "<link rel='stylesheet' media='screen' href='$loadStyle' />";
+            echo $html;
+        } else {
+            die("Stylesheet not found: $loadStyle");
+            // show 404 here
+        }
+    }
+
+    return $html;
+}
+
+// pass in the array of scripts to load, 
+// name must be same as that of script without extension
+function LoadScripts(array $scripts) : string
+{
+    $html = '';
+    foreach($scripts as $script) {
+        $loadScript = 'view/static/js/'.$script.'.js';
+        if (is_file($loadScript)) {
+            $html .= "<link rel='stylesheet' media='screen' href='$loadScript' />";
+            echo $html;
+        } else {
+            die("Stylesheet not found: $loadScript");
+            // show 404 here
+        }
+    }
+
+    return $html;
 }
 
 ?>
